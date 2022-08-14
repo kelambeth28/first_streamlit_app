1f49864ba7d2cb32c08225b6fc76808ca35dbc55
SUNEESH KELAMBETH <111200178+kelambeth28@users.noreply.github.com>
2022-08-14 16:14:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,37 +44,3 @@ try:
 
 except URLError as e:
   streamlit.error()  
-
-try:
-#DISPLAY ALL FRUITS
-
-  streamlit.text("The Fruit load list contains:")
-#sno related functions
-  def get_fruit_load_list():
-   with my_cnx.cursor() as my_cur:
-      my_cur.execute("SELECT * from fruit_load_list")
-      return my_cur.fetchall()
-#Add a button to show the list
-  if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows=get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)
-  
-#ADDING FRUITS
-
-
-  
-  def get_fruit_add_list(new_fruit):
-   with my_cnx.cursor() as my_cur:
-      my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('"+new_fruit+"')")
-      return "Thanks for adding "+ new_fruit
-    
-  #Add a button to show the list
-  fruit_add = streamlit.text_input('What fruit would you like to add?')
-  if streamlit.button('Add a Fruit to the List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    back_from_function=get_fruit_add_list(fruit_add)
-    streamlit.text=(back_from_function)
-except URLError as e:
-  streamlit.error()  
- 
