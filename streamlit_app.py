301adb0578726e2d43262ae1e775ee5e1766a950
SUNEESH KELAMBETH <111200178+kelambeth28@users.noreply.github.com>
2022-08-14 15:29:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+def get_fruitywise_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
@@ -29,13 +35,13 @@ try:
   if not fruit_choice:
       streamlit.error("Please select a fruit to get the information")
   else:
-      
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+      back_from_function=get_fruitywise_data(fruit_choice)
+      #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
       # streamlit.text(fruityvice_response.json())
       # write your own comment -what does the next line do? 
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       # write your own comment - what does this do?
-      streamlit.dataframe(fruityvice_normalized)
+      streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
   
