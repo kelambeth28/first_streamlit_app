4c4b2bf4a9511da30d49f3c0c4de7ddde3ac31aa
SUNEESH KELAMBETH <111200178+kelambeth28@users.noreply.github.com>
2022-08-14 15:38:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,14 +44,30 @@ try:
       streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
-  
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur= my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+ 
+#DISPLAY ALL FRUITS
+
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur= my_cnx.cursor()
+# my_cur.execute("SELECT * from fruit_load_list")
+# my_data_rows = my_cur.fetchall()
 
 streamlit.text("The Fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#sno related functions
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("SELECT * from fruit_load_list")
+      return my_cur.fetchall()
+#Add a button to show the list
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows=get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+    
+  
+
+
+#ADDING FRUITS
 fruit_add =''
 
 fruit_add = streamlit.text_input('What fruit would you like to add?')
