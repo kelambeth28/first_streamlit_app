0dba2409f3926e1d0cd79df0bb21dc1989ea76a0
SUNEESH KELAMBETH <111200178+kelambeth28@users.noreply.github.com>
2022-08-14 16:43:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ from urllib.error import URLError
 
 streamlit.title ('🥣 My Parents now healthy diner')
 streamlit.header('🥗 Breakfast Menu')
-streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Omega 3  Blueberry Oatmeal')
+streamlit.text('🥑 Kale, Spinach  Rocket Smoothie')
 streamlit.text('🍞 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
