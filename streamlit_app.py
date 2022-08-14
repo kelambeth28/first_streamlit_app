6ffd04e3339733a8c8490d8afda09775a145aa59
SUNEESH KELAMBETH <111200178+kelambeth28@users.noreply.github.com>
2022-08-14 13:48:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit;
 import pandas;
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
 
 streamlit.title ('🥣 My Parents now healthy diner');
 streamlit.header('🥗 Breakfast Menu')
@@ -19,4 +18,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
+
 
