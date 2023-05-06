6ea3cb698beb57fe16d74ce6e7e96e869a448470
SUNEESH KELAMBETH <111200178+kelambeth28@users.noreply.github.com>
2023-05-06 14:53:21 -0500

    Update streamlit_app.py
    Build section added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,10 @@ import streamlit
 
 
 streamlit.title("My Parents New Healthy Diner")
-streamlit.header('Breakfast Manu')
+streamlit.header('Breakfast Favorites')
 streamlit.text("🥣 Omega 3  & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinatch & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞  Avacado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
