15d69cd43598ac053e809624a6fbe31460d1fd5f
SUNEESH KELAMBETH <111200178+kelambeth28@users.noreply.github.com>
2023-05-06 14:49:40 -0500

    Update streamlit_app.py
    Added icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header('Breakfast Manu')
-streamlit.text("Omega 3  & Blueberry Oatmeal")
-streamlit.text("Kale, Spinatch & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text("🥣 Omega 3  & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinatch & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞  Avacado Toast")
