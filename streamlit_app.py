faa9e080c777958fa5fa05ea181636f06667ca05
SUNEESH KELAMBETH <111200178+kelambeth28@users.noreply.github.com>
2023-05-07 13:28:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,4 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text=fruityvice_response.json())
+streamlit.text(fruityvice_response.json())
